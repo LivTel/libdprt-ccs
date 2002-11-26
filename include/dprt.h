01fa60ff5f2ca89c4b6b4fed2359ebfc7cceb76c
Chris Mottram <C.Mottram@ljmu.ac.uk>
2002-11-26 18:13:03 +0000

    Integrated librjs_dprt.

--- include/dprt.h
+++ include/dprt.h
@@ -1,5 +1,5 @@
 /* dprt.h
-** $Header: /space/home/eng/cjm/cvs/libdprt-ccs/include/dprt.h,v 0.3 2002-05-20 11:01:40 cjm Exp $
+** $Header: /space/home/eng/cjm/cvs/libdprt-ccs/include/dprt.h,v 0.4 2002-11-26 18:13:03 cjm Exp $
 */
 #ifndef DPRT_H
 #define DPRT_H
@@ -23,10 +23,13 @@
 #define DPRT_ERROR_STRING_LENGTH	256
 
 /* function declarations */
-extern void DpRt_Initialise(void);
+extern int DpRt_Initialise(void);
+extern int DpRt_Shutdown(void);
 extern int DpRt_Calibrate_Reduce(char *input_filename,char **output_filename,double *mean_counts,double *peak_counts);
 extern int DpRt_Expose_Reduce(char *input_filename,char **output_filename,double *seeing,double *counts,double *x_pix,
 		       double *y_pix,double *photometricity,double *sky_brightness,int *saturated);
+extern int DpRt_Make_Master_Bias(char *directory_name);
+extern int DpRt_Make_Master_Flat(char *directory_name);
 extern void DpRt_Set_Abort(int value);
 extern int DpRt_Get_Property(char *keyword,char **value_string);
 extern int DpRt_Get_Property_Integer(char *keyword,int *value);
